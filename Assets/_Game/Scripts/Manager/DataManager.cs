e297d20d347fd98639198caa9fa10735ee787106
sonerror <nguyenvansonawm@gmail.com>
2024-07-03 08:34:20 +0700

    Convert data to json

--- Assets/_Game/Scripts/Manager/DataManager.cs
+++ Assets/_Game/Scripts/Manager/DataManager.cs
@@ -10,41 +10,42 @@ public class DataManager : Singleton<DataManager>
 {
     public bool isLoaded = false;
     public PlayerData playerData;
-    public const string PLAYER_DATA = "PLAYER_DATA";
+    private const string PLAYER_DATA_PATH = "/GameData/PlayerData.json";
+    private static string systemPath = Application.dataPath + PLAYER_DATA_PATH;
 
     private void OnApplicationPause(bool pause) { SaveData(); }
     private void OnApplicationQuit() { SaveData(); }
 
     public void LoadData()
-    {   
+    {
         Debug.Log("START LOAD DATA");
-        string d = PlayerPrefs.GetString(PLAYER_DATA, "");
-        if (d != "")
-        {
-            playerData = JsonUtility.FromJson<PlayerData>(d);
-        }
-        else
+        playerData = DataUtilities.LoadData<PlayerData>(systemPath);
+
+        if (playerData == null)
         {
             playerData = new PlayerData();
             FirstLoad();
         }
         isLoaded = true;
     }
+
     public void SaveData()
     {
         if (!isLoaded) return;
-        string json = JsonUtility.ToJson(playerData);
-        PlayerPrefs.SetString(PLAYER_DATA, json);
+        DataUtilities.SaveData(playerData, systemPath);
         Debug.Log("SAVE DATA");
     }
+
     void FirstLoad()
     {
         SaveLevelDataModels();
     }
+
     public void SaveLevelDataModels()
     {
-        DataManager.Ins.playerData.levelDataModels = LevelManager.Ins.levelDatas.level3D.Select(ld => new LevelDataModel(ld.levelID, 0)).ToList();
-        DataManager.Ins.SaveData();
+        playerData.levelDataModels = LevelManager.Ins.levelDatas.level3D
+            .Select(ld => new LevelDataModel(ld.levelID, 0)).ToList();
+        SaveData();
     }
 }
 [System.Serializable]
---
+++ Assets/_Game/Scripts/Other/DataUtilities.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class DataUtilities : MonoBehaviour
+{
+    private const string DEFAULT_PATH = "/GameData";
+    private const string DEFAULT_FILE_NAME = "PlayerData.json";
+    private static string systemPath = Application.dataPath + DEFAULT_PATH + "/" + DEFAULT_FILE_NAME;
+
+    public static void SaveData<T>(T data, string filePath = null)
+    {
+        if (filePath == null)
+        {
+            filePath = systemPath;
+        }
+
+        if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+        }
+
+        string jsonData = JsonUtility.ToJson(data);
+        File.WriteAllText(filePath, jsonData);
+        Debug.Log("Đã lưu thành công Data tại: " + filePath);
+    }
+
+    public static T LoadData<T>(string filePath = null)
+    {
+        if (filePath == null)
+        {
+            filePath = systemPath;
+        }
+
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            Debug.Log("Đã tải thành công Data tại: " + filePath);
+            return JsonUtility.FromJson<T>(jsonData);
+        }
+        else
+        {
+            Debug.LogError("Không tìm thấy đường dẫn: " + filePath);
+            return default(T);
+        }
+    }
+
+    public static void UpdateData<T>(T newData, string filePath = null)
+    {
+        if (filePath == null)
+        {
+            filePath = systemPath;
+        }
+
+        if (File.Exists(filePath))
+        {
+            string jsonData = JsonUtility.ToJson(newData);
+            File.WriteAllText(filePath, jsonData);
+        }
+        else
+        {
+            Debug.LogError("Không tìm thấy đường dẫn: " + filePath);
+        }
+    }
+
+    public static void DeleteData(string filePath = null)
+    {
+        if (filePath == null)
+        {
+            filePath = systemPath;
+        }
+
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+        else
+        {
+            Debug.LogError("Không tìm thấy đường dẫn: " + filePath);
+        }
+    }
+}
