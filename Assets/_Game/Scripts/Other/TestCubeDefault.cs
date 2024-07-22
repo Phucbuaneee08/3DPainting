55c23fe683c96f245cfa6900c0d2359c765064a9
Phucbuaneee08 <92803855+Phucbuaneee08@users.noreply.github.com>
2024-07-22 17:17:11 +0700

    fix level

---
+++ Assets/_Game/Scripts/Other/TestCubeDefault.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCubeDefault : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void DisplayIdCubeDefault()
+    {
+        List<int> idCubeDefault = new List<int>();
+        List<Cube> cubes = LevelManager.Ins.Cubes;
+        foreach (Cube cube in cubes)
+        {
+            if (cube.IsState(CubeState.Default))
+            {
+                idCubeDefault.Add(cube.ID);
+            }
+        }
+        if (idCubeDefault.Count > 0)
+        {
+            string idsString = string.Join(",", idCubeDefault);
+            Debug.Log("Default Cube IDs: " + idsString);
+        }
+        else
+        {
+            Debug.Log("No Default Cubes found.");
+        }
+
+    }
+}
